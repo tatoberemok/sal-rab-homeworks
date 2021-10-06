8b66f9cd700e23a2479b114640447a98741f8bd9
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-06 15:06:59 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -33,7 +33,7 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-    if (productsSum >= discountMinSum) {discountSum = 10} {discountSum = 0};
+    if (productsSum >= discountMinSum) {discountSum = 10} else {discountSum = 0};
     // Конец решения задания №2.2.
 
     return discountSum;
