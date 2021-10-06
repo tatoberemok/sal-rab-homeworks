126cb5a0e03a9ea5f53896a595ab8ba6e46161b0
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-06 09:33:58 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -19,7 +19,7 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
        message += " ";
     // добавьте в конец переменной message текст 'теперь в корзине!'
-       message = message + "₽ теперь в корзине!";
+       message = message + " ₽ теперь в корзине!";
     // выведите переменную message в консоль
     console.log(message);
     // Конец решения задания №1.1.
