21dd8e88232814cd8c3eacc0e8e8bf83009b0811
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-06 12:59:44 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,13 +9,13 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-    if (productsSum == 0) {shippingSum =0};
+    if (productsSum == 0) {shippingSum = 0};
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     if (productsSum >= freeShippingMinSum) {shippingSum = 0};
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if (productsSum > 0 and productsSum < freeShippingMinSum) {shippingSum = shippingPrice};
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {shippingSum = shippingPrice};
     // Конец решения задания №2.1.
 
     return shippingSum;
