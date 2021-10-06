2543351999a2d1a207c9d9fe3d52709af542b6b3
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-06 09:38:11 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -9,15 +9,15 @@ function addInCartMessage(title, price) {
     // присвойте переменной message значение переменной productName
        message = productName
     // добавьте в конец переменной message пробел
-    //   message += " ";
+       message += " ";
     // добавьте в конец переменной message текст 'за'
-       message = message + " за";
+       message = message + "за";
     // добавьте в конец переменной message пробел
        message += " ";
     // добавьте в конец переменной message значение переменной productPrice
        message = message + productPrice;
     // добавьте в конец переменной message пробел
-       message = message + " ₽ ";
+       message = message + " ";
     // добавьте в конец переменной message текст 'теперь в корзине!'
        message = message + "теперь в корзине!";
     // выведите переменную message в консоль
