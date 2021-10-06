6047e7156ecb7df9bc4313c55007ffccbb447494
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-06 12:55:04 +0300

    Update solution.js

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,16 +6,16 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
-    let shippingSum
+    let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-    if (productsSum == 0) {shippingSum =0}
+    if (productsSum == 0) {shippingSum =0};
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    if (productsSum >= freeShippingMinSum) {shippingSum = 0}
+    if (productsSum >= freeShippingMinSum) {shippingSum = 0};
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if (productsSum > 0 and productsSum < freeShippingMinSum) {shippingSum = shippingPrice}
+    if (productsSum > 0 and productsSum < freeShippingMinSum) {shippingSum = shippingPrice};
     // Конец решения задания №2.1.
 
     return shippingSum;
