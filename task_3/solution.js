0148942fa3220aa13f0c6a177d3427b08a0552b1
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-11 10:38:15 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -26,7 +26,7 @@ function renderCartItem(item) {
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
        itemCountText += " ₽ = ";
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-       let sum = product.count+product.price;
+       let sum = product.count*product.price;
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
        itemCountText += sum;
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
