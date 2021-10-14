e5f0845f70b48b8ce728e981be9706c8a0de7100
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-14 12:20:04 +0300

    Update solution.js

--- task_4/solution.js
+++ task_4/solution.js
@@ -17,7 +17,7 @@ function parseProducts(json) {
 function renderProductsCards(json){
     clearProducts();
     let products = JSON.parseProducts(json);
-    length = products.length;
+    let length = products.length;
     for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
     }
