c80f475f41582a663078105ef71a54f0db9234ea
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-15 15:34:41 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -47,7 +47,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-  //let jsonData = JSON.stringify(data);
-    let jsonData = JSON.stringify({data: data});
+    let jsonData = JSON.stringify(data);
+  //let jsonData = JSON.stringify({data: data});
     return jsonData;
 }
