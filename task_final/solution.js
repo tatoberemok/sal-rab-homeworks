0f538560f5b7972479165d95fce5dc0f323ab66f
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-16 14:04:12 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,10 +42,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
         data.goods.push(goods[i].count);
     }
-    order.address = JSON.stringify({address});
-    order.address  = {str, dom, entrance, floor, quart};
-    order.address = "ул. " + address.str + " " + ",дом" + address.dom + " " + "," + address.entrance + ",подъезд" + " " + address.floor + " ,кв" + address.quart;
-    data.order.address = address;
+    data.order.address = JSON.stringify({address});
+ // order.address  = {str, dom, entrance, floor, quart};
+ // order.address = "ул. " + address.str + " " + ",дом" + address.dom + " " + "," + address.entrance + ",подъезд" + " " + address.floor + " ,кв" + address.quart;
+ // data.order.address = address;
     data.order.sum = order.sum;
     data.client = sendRequest(name) + " " + sendRequest(phone);
     data.order = "Address:" + data.order.address + "Sum:" + data.order.sum;
