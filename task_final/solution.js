da462c5898973a76a618173a59ecb1165745e399
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-16 20:28:00 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,8 +42,8 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push(goods[i].title);
         data.goods.push(goods[i].count);
     }
-    let myAddress = JSON.stringify(address);
-    data.order.address.toString();
+    let order = JSON.stringify(address);
+ // data.order.address.toString();
     data.order.address = address;
     data.order.sum = sum;
     data.client = sendRequest(name) + " " + sendRequest(phone);
