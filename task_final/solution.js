40e0481b6142781fa135d54201665a0741931952
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-17 10:29:05 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -45,7 +45,7 @@ function sendRequest(name, phone, address, goods, sum) {
     
 
     address.toString();
-//  let address = {street, house, entrance, floor, flat};
+//  let order = {address: {street, house, entrance, floor, flat},sum};
     address = "";
     address += "ул. " + address.street;
     address += "," + "дом" + address.house;
