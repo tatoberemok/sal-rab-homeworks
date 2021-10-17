18df76992e8e0802c374c80ce3d7b98a4eae45cb
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-17 17:50:08 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,9 +36,9 @@
 function sendRequest(name, phone, address, goods, sum) {
     let data = {client: {name, phone}, order: {address, sum}, goods: []};
     let countOfGoods = goods.length;
-    let object = goods.title + goods.count;
     for (let i = 0; i < countOfGoods; i ++) {
-           data.goods.push(goods[i].object);
+        let object = goods[i].title + goods[i].count;   
+        data.goods.push(goods[i].object);
                }
  
     data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
