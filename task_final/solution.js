c2d999d4c119e3a13f1d999064d1ee8e64b9db1b
tatoberemok <91879090+tatoberemok@users.noreply.github.com>
2021-10-17 17:10:46 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -42,7 +42,7 @@ function sendRequest(name, phone, address, goods, sum) {
        
     }
    
-    data.order.address = `ул. ${address.street}`дом `${address.house}` , `${address.entrance}` ,подъезд `${address.floor}` ,кв `${address/flat}`;
+    data.order.address = `ул. ${address.street}`;
     console.log(data.order.address);
     data.order.sum = sum;
 
